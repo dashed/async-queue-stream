f4320eba32229101190b6ffe15aa97650a94bee2
Alberto Leal <mail4alberto@gmail.com>
2014-01-11 02:28:21 -0500

    init commit

---
+++ README.md
@@ -0,0 +1,108 @@
+async-queue-stream
+==================
+
+Wrapper for [through](https://github.com/dominictarr/through) stream to use [async.queue](https://github.com/caolan/async#queueworker-concurrency) under the hood.
+
+It takes an asynchronous function and queue stream chunks. Then the queue executes the asynchronous function up to the concurrency threshold.
+
+Any error will be emitted to `opts.error_event` event. By default, the stream will not stop on error; this can be configured via `opts.stop_on_error`.
+
+## asyncqueue(write_fn [[, end_fn], options])
+
+**Arguments**
+
+* `write_fn(data, callback)` - an asnynchronous function that will be wrapped into a through stream
+
+    `data` is the queued stream chunk.
+
+    There are three ways to invoke callback:
+    * `callback(null, transformedData)` - emit data
+    * `callback(error)` - emit error
+    * `callback()` - drop data (don't emit to next stream)
+
+* `end_fn()` - a function that will be invoked when no more data will be provided.
+
+* `options` - an object containing options
+    * `options.concurrency` - concurrency argument to [async.queue](https://github.com/caolan/async#queueworker-concurrency). ***Default:*** 1
+
+    * `options.error_event` - event name used to emit the error from callback via the asynchronous function. ***Default:*** 'failure'
+
+    * `options.stop_on_error` - boolean value for `asyncqueue` to stop queuing any more stream chunks if callback via the asynchronous function has emitted an error. ***Default:*** false
+
+        **Note:** Any tasks already executed will be able to complete.
+
+Example
+=======
+
+```js
+var es = require('event-stream');
+var qasync = require('queue-async-stream');
+
+// something that returns stream using queue-async-stream internally
+var plugin = function(filter, filter_func) {
+
+    if(filter_func == void 0)
+        filter_func = function(n) { return n; };
+
+    return qasync(function (data, cb) {
+
+        if(filter_func(data) == filter) {
+            setTimeout(function() {
+
+                return cb(new Error(filter+''));
+            }, 1000);
+            return;
+        }
+
+        if(data === 2) {
+            setTimeout(function() {
+                return cb(null, data);
+            }, 2000);
+            return;
+        }
+
+        console.log('caught in plugin: ' + data)
+        return cb(null, data);
+
+   }, {concurrency: 2});
+};
+
+
+es.readArray([1,2,3,4,5])
+    .pipe(plugin(3))
+        .on('failure', console.log)
+    .pipe(es.through(function(n) {
+
+        console.log('caught in es.through: ' + n);
+
+        this.emit('data', n);
+    }));
+/**
+Output:
+    caught in plugin: 1
+    caught in es.through: 1
+    [Error: 3]
+    caught in plugin: 4
+    caught in es.through: 4
+    caught in plugin: 5
+    caught in es.through: 5
+    caught in es.through: 2
+
+
+With stop_on_error:true, output is:
+    caught in plugin: 1
+    caught in es.through: 1
+    [Error: 3]
+    caught in es.through: 2
+ */
+```
+
+To Do
+=====
+
+1. Be able to pass opts/hooks to async.queue (e.g. drain, empty, etc)
+
+License
+=======
+
+MIT. See LICENSE file.
---
+++ index.js
@@ -0,0 +1,211 @@
+var through = require('through');
+
+/// start async stuff ////
+// broken async.queue out at commit f01b3992fb
+// from: https://github.com/caolan/async
+var async = {};
+
+function only_once(fn) {
+    var called = false;
+    return function() {
+        if (called) throw new Error("Callback was already called.");
+        called = true;
+        fn.apply(root, arguments);
+    }
+}
+
+async.setImmediate = setImmediate;
+
+var _each = function (arr, iterator) {
+    if (arr.forEach) {
+        return arr.forEach(iterator);
+    }
+    for (var i = 0; i < arr.length; i += 1) {
+        iterator(arr[i], i, arr);
+    }
+};
+
+async.queue = function (worker, concurrency) {
+    if (concurrency === undefined) {
+        concurrency = 1;
+    }
+    function _insert(q, data, pos, callback) {
+      if(data.constructor !== Array) {
+          data = [data];
+      }
+      _each(data, function(task) {
+          var item = {
+              data: task,
+              callback: typeof callback === 'function' ? callback : null
+          };
+
+          if (pos) {
+            q.tasks.unshift(item);
+          } else {
+            q.tasks.push(item);
+          }
+
+          if (q.saturated && q.tasks.length === concurrency) {
+              q.saturated();
+          }
+          async.setImmediate(q.process);
+      });
+    }
+
+    var workers = 0;
+    var q = {
+        tasks: [],
+        concurrency: concurrency,
+        saturated: null,
+        empty: null,
+        drain: null,
+        push: function (data, callback) {
+          _insert(q, data, false, callback);
+        },
+        unshift: function (data, callback) {
+          _insert(q, data, true, callback);
+        },
+        process: function () {
+            if (workers < q.concurrency && q.tasks.length) {
+                var task = q.tasks.shift();
+                if (q.empty && q.tasks.length === 0) {
+                    q.empty();
+                }
+                workers += 1;
+                var next = function () {
+                    workers -= 1;
+                    if (task.callback) {
+                        task.callback.apply(task, arguments);
+                    }
+                    if (q.drain && q.tasks.length + workers === 0) {
+                        q.drain();
+                    }
+                    q.process();
+                };
+                var cb = only_once(next);
+                worker(task.data, cb);
+            }
+        },
+        length: function () {
+            return q.tasks.length;
+        },
+        running: function () {
+            return workers;
+        }
+    };
+    return q;
+};
+
+/// end async stuff ////
+
+
+//// meat ////
+
+/**
+ * stream wrapper of through using async.queue under the hood
+ *
+ * queueAsync(write_fn[[, end_fn], opts])
+ */
+var queueAsync = function (write_fn, end_fn, opts) {
+
+    opts = opts || {};
+
+    // support queueAsync(write_fn, opts)
+    if(typeof end_fn == 'object' && end_fn.constructor == Object) {
+      opts = end_fn;
+      end_fn = function() {this.emit('end');};
+    }
+
+    opts.concurrency = opts.concurrency || 1;
+
+    opts.error_event = opts.error_event || 'failure';
+    opts.stop_on_error = !!opts.stop_on_error;
+
+    write_fn = write_fn || function(data, cb) {cb(null,data);};
+    end_fn = end_fn || function() {this.emit('end');};
+
+    // toggle to stop on error
+    var error_raised = false;
+
+    // set up queue and its hard worker
+    var worker = function(_work, callback) {
+
+      var _args, _f, _this, _cb;
+
+      _f = _work.f;
+      _this = _work._this;
+      _args = _work._args || [];
+
+      // wrap _write_callback to call async.queue callback
+      var _w_cb = _work.write_cb;
+
+      _args.push(_w_cb.bind({}, _work.write_cb_stream, callback));
+
+      // _args.push(callback);
+      if(error_raised !== true)
+        return _f.apply(_this, _args);
+    };
+
+    var queue = async.queue(worker, opts.concurrency);
+
+    // callback signature ref.
+    //
+    // callback(); - drop
+    // callback(null, null); - emit null as data
+    // callback(null, data); - emit data
+    // callback(err, null); - emit err
+
+    // _write_callback is called when write_fn wants to push err or data
+    // reminder: cb is async.queue callback
+    var _write_callback = function(stream, cb, err, data) {
+        if(err) {
+            return cb(err);
+        }
+
+        if(data !== void 0) {
+            return stream.emit('data', data), cb();
+        }
+    }
+
+    // _work_callback is called when
+    var _work_callback = function(err) {
+        if(err) {
+            if(opts.stop_on_error && !error_raised) error_raised = true;
+
+            return this.emit(opts.error_event, err);
+        }
+    }
+
+    var _write = function(data) {
+
+        // _write_callback_binded = _write_callback.bind(this);
+        _work_callback_binded = _work_callback.bind(this);
+
+        var work_package = {};
+        work_package.f = write_fn;
+        work_package._this = {};
+        work_package._args = [data];
+
+        work_package.write_cb = _write_callback;
+        work_package.write_cb_stream = this;
+
+        if(error_raised !== true)
+          queue.push(work_package, _work_callback_binded);
+
+    };
+
+    var _empty = false;
+
+    var stream = through(_write, function(){ _empty = true; });
+
+    // Clean up
+    queue.drain = function(err) {
+        if(_empty) {
+            end_fn.bind(stream)();
+        }
+    }
+
+    return stream;
+};
+
+module.exports = queueAsync;
